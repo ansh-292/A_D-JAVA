f234a16681eb20b6524df8692efcc8ac7acb0ff0
ansh-292 <ansh2927282@gmail.com>
2025-08-12 10:52:18 +0530

    Starting of java(Range of data type)

---
+++ App.java
@@ -0,0 +1,42 @@
+// public class App {
+//     public static void main(String[] args) throws Exception {
+//         //byte
+//         System.out.println("Byte minimum value: " + Byte.MIN_VALUE);
+//         System.out.println("Byte maxmum value: " + Byte.MAX_VALUE);
+
+//         //short
+//         System.out.println("Short minimum value: " + Short.MIN_VALUE);
+//         System.out.println("Short maxmum value: " + Short.MAX_VALUE);
+        
+//         //integer
+//         System.out.println("Integer minimum value: " + Integer.MIN_VALUE);
+//         System.out.println("Integer maxmum value: " + Integer.MAX_VALUE);
+        
+//         //long
+//         System.out.println("Long minimum value: " + Long.MIN_VALUE);
+//         System.out.println("Long maxmum value: " + Long.MAX_VALUE);
+        
+//         //Float
+//         System.out.println("Float minimum value: " + Float.MIN_VALUE);
+//         System.out.println("Float maxmum value: " + Float.MAX_VALUE);
+        
+//         //Double
+//         System.out.println("Double minimum value: " + Double.MIN_VALUE);
+//         System.out.println("Double maxmum value: " + Double.MAX_VALUE);
+        
+//         //Character
+//         System.out.println("Char minimum value: " + (int)Character.MIN_VALUE);
+//         System.out.println("Char maxmum value: " + (int)Character.MAX_VALUE);
+        
+//         //Boolean
+//         System.out.println("Boolean value: " + Boolean.TRUE + "and" + Boolean.FALSE);
+
+//     }
+// }
+
+
+public class Main {
+    public static void main(String[] args) {
+        
+    }
+}
